e79919287414223311e8706178d7c35919e5a2e1
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-03-23 10:22:44 +0200

    backpack controllers now add routes to backpack/custom.php

--- src/Creators/BackpackControllerCreator.php
+++ src/Creators/BackpackControllerCreator.php
@@ -2,6 +2,8 @@
 
 namespace Andyabih\JsonToLaravelMigrations\Creators;
 
+use Error;
+
 class BackpackControllerCreator
 {
     protected $modelName, $controllers;
@@ -12,13 +14,14 @@ class BackpackControllerCreator
     }
 
     public function create()
-    { 
+    {
         if (!file_exists($this->getLocation())) {
             mkdir($this->getLocation(), 0775);
         }
-       
+
         foreach ($this->controllers as $table => $controller) {
             $this->createController($table, $controller);
+            $this->addRoute();
         }
     }
 
@@ -36,8 +39,54 @@ class BackpackControllerCreator
         $stub     = $this->createStub($modelName, $controller);
         $path     = $this->getPath($filename);
 
-
         file_put_contents($path, $stub);
+    }
+
+    private function addRoute()
+    {
+        $path = 'routes/backpack/custom.php';
+        $disk_name = config('backpack.base.root_disk_name');
+        $disk = \Storage::disk($disk_name);
+        $code = 'test';
+        // Route::crud('category', 'CategoryCrudController')
+        if ($disk->exists($path)) {
+            $old_file_path = $disk->path($path);
+
+            // insert the given code before the file's last line
+            $file_lines = file($old_file_path, FILE_IGNORE_NEW_LINES);
+
+            $end_line_number = $this->customRoutesFileEndLine($file_lines);
+            $file_lines[$end_line_number + 1] = $file_lines[$end_line_number];
+            $file_lines[$end_line_number] = '    ' . $code;
+            $new_file_content = implode(PHP_EOL, $file_lines);
+
+            if (!$disk->put($path, $new_file_content)) {
+                throw new Error('Could not write to file: ' . $path);
+            }
+        }
+    }
+
+    private function customRoutesFileEndLine($file_lines)
+    {
+        // in case the last line has not been modified at all
+        $end_line_number = array_search('}); // this should be the absolute last line of this file', $file_lines);
+
+        if ($end_line_number) {
+            return $end_line_number;
+        }
+
+        // otherwise, in case the last line HAS been modified
+        // return the last line that has an ending in it
+        $possible_end_lines = array_filter($file_lines, function ($k) {
+            return strpos($k, '});') === 0;
+        });
+
+        if ($possible_end_lines) {
+            end($possible_end_lines);
+            $end_line_number = key($possible_end_lines);
+
+            return $end_line_number;
+        }
     }
 
     private function generateModelName($table)
@@ -50,7 +99,8 @@ class BackpackControllerCreator
         return sprintf('%sCrudController.php', $modelName);
     }
 
-    private function createStub($modelName, $controller) {
+    private function createStub($modelName, $controller)
+    {
 
         $stub = $this->getStub();
         $stub = str_replace("{{modelName}}", $modelName, $stub);
@@ -59,17 +109,18 @@ class BackpackControllerCreator
         return $stub;
     }
 
-    private function generateOperations($controller) {
+    private function generateOperations($controller)
+    {
 
         $finalFields = [];
 
-        foreach($controller as $column => $fields){
+        foreach ($controller as $column => $fields) {
             $fields['name'] = $column;
 
             $finalField = [];
 
-            foreach($fields as $key => $value){
-                $finalField[] = "\t'{$key}' => " . "'{$value}'"; 
+            foreach ($fields as $key => $value) {
+                $finalField[] = "\t'{$key}' => " . "'{$value}'";
             }
 
             array_push($finalFields, "\n\t\t\t[\n\t\t\t" . implode(",\n\t\t\t", $finalField) . "\n\t\t\t]");
@@ -83,11 +134,13 @@ class BackpackControllerCreator
         return file_get_contents(__DIR__ . './../stubs/crud-controller.stub');
     }
 
-    private function getPath($filename) {
+    private function getPath($filename)
+    {
         return base_path() . '/app/Http/Controllers/Admin/' . $filename;
     }
 
-    private function getLocation(){
+    private function getLocation()
+    {
         return base_path() . '/app/Http/Controllers/Admin';
     }
 
@@ -121,7 +174,7 @@ class BackpackControllerCreator
     //     return $rootNamespace.'\Http\Controllers\Admin';
     // }
 
-    
+
 
     // protected function getAttributes($model)
     // {
@@ -216,5 +269,5 @@ class BackpackControllerCreator
     //     return $stub;
     // }
 
-    
+
 }
